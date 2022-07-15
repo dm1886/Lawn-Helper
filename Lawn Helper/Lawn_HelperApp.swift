fe8c83bddae6bac0f074e2685becf571805aec10
Damiano Miazzi <damianomiazzi@gmail.com>
2022-07-15 11:40:48 +0800

    Initial Commit

---
+++ Lawn Helper/Lawn_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lawn_HelperApp.swift
+//  Lawn Helper
+//
+//  Created by Damiano Miazzi on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Lawn_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
